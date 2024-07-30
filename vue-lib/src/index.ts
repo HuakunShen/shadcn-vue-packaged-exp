3177f0beab22257f54637e8f7278e76c9f27a2b7
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-30 19:56:54 -0400

    Add carousel

--- vue-lib/src/index.ts
+++ vue-lib/src/index.ts
@@ -2,5 +2,5 @@ import "./assets/index.css";
 
 export * from "./components/ui/accordion";
 export * from "./components/ui/button";
-export * from "./components/ui/command";
 export * from "./components/ui/dialog";
+export * from "./components/ui/carousel";
